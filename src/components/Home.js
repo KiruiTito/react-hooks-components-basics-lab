4918c8fdf8e7cddc3012eadbddc7dc313dfc4d9e
KiruiTito <titokirui402@gmail.com>
2023-04-23 09:43:45 +0300

    Completed assignment

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio Site!</h1>
+      <p>This is my portfolio site. Please take a look around!</p>
+    </div>
+  );
+}
+
+export default Home;
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Link 1</li>
+        <li>Link 2</li>
+        <li>Link 3</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
